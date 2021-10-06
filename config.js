ca4e1117b40b90b2282192904d60befdff9cf01e
vikas rathour <vikasrathour162@gmail.com>
2021-10-07 00:13:48 +0530

    first uploading

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports ={
+    serviceID : "VAbe62448177c5442cf6f8b6eebbb06508",
+    accountSID : "AC26aa156f67514b358091d2e60b7f94a8",
+    authToken :"547cdb04a9a8644c7b4964887d388fcb"
+}
